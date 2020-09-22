04fd08044765e3b886d00919d1d1964f896dcfc7
vitvara <vitvara1@gmail.com>
2020-09-22 11:45:38 +0700

    fix doctest_ex.py :doctest space and create tictactoe (hardcode)

--- ex5/doctest_ex.py
+++ ex5/doctest_ex.py
@@ -5,6 +5,9 @@ def string_interleave(s1, s2):
         s1 (string): some word
         s2 (string): some word
 
+    Raises:
+        TypeError: Input s1 and s2 must be str.
+
     Returns:
         string: s1 swap with s2 or s2 swap with s1
 
@@ -16,7 +19,19 @@ def string_interleave(s1, s2):
     'SHaewlalsodee Thailand'
     >>> string_interleave("Mine", "Thai")
     'TMhianie'
+    >>> string_interleave(1234, "Thai")
+    Traceback (most recent call last):
+        ...
+    TypeError: s1 and s2 must be string!
+    >>> string_interleave("Test", 1234)
+    Traceback (most recent call last):
+        ...
+    TypeError: s1 and s2 must be string!
     """
+    if type(s1) != str or type(s2) != str:
+        # If yes, Raise the TypeError Exception
+        raise TypeError("s1 and s2 must be string!")
+
     if len(s2) < len(s1):
         for i in range(len(s2)):
             s1 = s1[:2*i+1] + s2[i] + s1[2*i+1:]
@@ -34,6 +49,9 @@ def selective_sum(n, k):
         n (integer): number
         k (interger): select digit
 
+    Raises:
+        TypeError: Input s1 and s2 must be str.
+
     Returns:
         integer : sum of maxium digit
 
@@ -44,7 +62,17 @@ def selective_sum(n, k):
     25
     >>> selective_sum(12345, 10)
     15
+    >>> selective_sum("Test", 3)
+    Traceback (most recent call last):
+        ...
+    TypeError: n and k must be int!
+    >>> selective_sum(1234, "5")
+    Traceback (most recent call last):
+        ...
+    TypeError: n and k must be int!
     """
+    if type(n) != int or type(k) != int:
+        raise TypeError("n and k must be int!")
     list_ = []
     sum = 0
     if k > len(str(n)):
@@ -65,6 +93,9 @@ def list_intersect(l1, l2):
         l1 (list): list of number
         l2 (list): list of number
 
+    Raises:
+        TypeError: l1 and l2 must be list
+
     Returns:
         list : list of number in l1 that intersect l2
 
@@ -78,7 +109,18 @@ def list_intersect(l1, l2):
     []
     >>> list_intersect([9, 10, 11, 12], [5, 6, 9, 10, 7, 8]) # return [9, 10]
     [9, 10]
+    >>> list_intersect("HelloTest", [5, 6, 7, 8])
+    Traceback (most recent call last):
+        ...
+    TypeError: l1 and l2 must be list
+    >>> list_intersect([1, 2, 3, 5], "Find me!")
+    Traceback (most recent call last):
+        ...
+    TypeError: l1 and l2 must be list
+
     """
+    if type(l1) != list or type(l2) != list:
+        raise TypeError("l1 and l2 must be list")
     list_ = []
     for i in l1:
         for j in l2:
--- ex5/tic_tac_toe_completed.py
+++ ex5/tic_tac_toe_completed.py
@@ -1,15 +1,38 @@
-theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ', 'mid-L': ' ',
-            'mid-M': ' ', 'mid-R': ' ', 'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
-the_num_Board = {'top-L': '1', 'top-M': '2', 'top-R': '3', 'mid-L': '4',
-                 'mid-M': '5', 'mid-R': '6', 'low-L': '7', 'low-M': '8', 'low-R': '9'}
+import sys
+theBoard = {'1': ' ', '2': ' ', '3': ' ',
+            '4': ' ', '5': ' ', '6': ' ',
+            '7': ' ', '8': ' ', '9': ' '}
+the_num_Board = {'1': '1', '2': '2', '3': '3',
+                 '4': '4', '5': '5', '6': '6',
+                 '7': '7', '8': '8', '9': '9'}
+check_list = ([1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+              [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7])
 
 
 def printBoard(board):
-    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
     print('-+-+-')
-    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
     print('-+-+-')
-    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+
+
+def checkwin(board, turn):
+    """[summary]
+
+    Args:
+        lx ([type]): [description]
+        lo ([type]): [description]1
+    >>> checwin([1,2,3], [2,4,5])
+    X win1
+    """
+    for i in check_list:
+        if board[f"{i[0]}"] == " " or board[f"{i[1]}"] == " " or board[f"{i[2]}"] == " ":
+            continue
+        elif board[f"{i[0]}"] == board[f"{i[1]}"] == board[f"{i[2]}"]:
+            printBoard(theBoard)
+            print(f"{turn} WIN!!!")
+            sys.exit()
 
 
 turn = 'X'
@@ -22,8 +45,13 @@ for i in range(9):
     theBoard[move] = turn
     if turn == 'X':
         theBoard[move] = "X"
+        checkwin(theBoard, turn)
         turn = 'O'
+
     else:
         theBoard[move] = "O"
+        checkwin(theBoard, turn)
         turn = 'X'
+
+
 printBoard(theBoard)
