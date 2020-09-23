6f28f477a6501f671b19d3b755598ee1188c8600
vitvara <vitvara1@gmail.com>
2020-09-23 21:19:21 +0700

    format tic_tac_toe.py by remove line check and moveTurn

--- ex5/tic_tac_toe_completed.py
+++ ex5/tic_tac_toe_completed.py
@@ -1,84 +1,74 @@
-# import sys
-# theBoard = {'1': ' ', '2': ' ', '3': ' ',
-#             '4': ' ', '5': ' ', '6': ' ',
-#             '7': ' ', '8': ' ', '9': ' '}
-# the_num_Board = {'1': '1', '2': '2', '3': '3',
-#                  '4': '4', '5': '5', '6': '6',
-#                  '7': '7', '8': '8', '9': '9'}
-# check_list = ([1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
-#               [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7])
+import sys
+theBoard = {'1': ' ', '2': ' ', '3': ' ',
+            '4': ' ', '5': ' ', '6': ' ',
+            '7': ' ', '8': ' ', '9': ' '}
+the_num_Board = {'1': '1', '2': '2', '3': '3',
+                 '4': '4', '5': '5', '6': '6',
+                 '7': '7', '8': '8', '9': '9'}
+check_list = ([1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+              [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7])
 
 
-# def printBoard(board):
-#     """[summary]
+def printBoard(board):
+    """[summary]
 
-#     Args:
-#         board (Dict): dict of value on tic tac toe
-#     """
-#     print(board['1'] + '|' + board['2'] + '|' + board['3'])
-#     print('-+-+-')
-#     print(board['4'] + '|' + board['5'] + '|' + board['6'])
-#     print('-+-+-')
-#     print(board['7'] + '|' + board['8'] + '|' + board['9'])
-
-
-# def checkwin(board, turn):
-#     """Check player win
-
-#     Args:
-#         lx ([type]): [description]
-#         lo ([type]): [description]1
-#     >>> checwin([1,2,3], [2,4,5])
-#     X win1
-#     """
-#     for i in check_list:
-#         if board[f"{i[0]}"] == " " or board[f"{i[1]}"] == " " or board[f"{i[2]}"] == " ":
-#             continue
-#         elif board[f"{i[0]}"] == board[f"{i[1]}"] == board[f"{i[2]}"]:
-#             printBoard(theBoard)
-#             print(f"{turn} WIN!!!")
-#             sys.exit()
+    Args:
+        board (Dict): dict of value on tic tac toe
+    """
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
 
 
-# turn = 'X'
-# indexlist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# # loop 9 time
-# for i in range(9):
-#     printBoard(the_num_Board)
-#     print()
-#     printBoard(theBoard)
-#     print('Turn for ' + turn + '. Move on which space?')
-#     # check move that is not letter or more than 9
-#     while True:
-#         move = input()
-#         try:
-#             o = int(move)
+def checkwin(board, turn):
+    """Check player win
 
-#         except ValueError:
-#             print("Incrorect type")
-#             continue
-#         try:
-#             c = indexlist[o-1]
-#             break
-#         except IndexError:
-#             print("Type number 1-9 ")
-#             continue
-#         theBoard[move] = turn
-#     if turn == 'X':
-#         theBoard[move] = "X"
-#         checkwin(theBoard, turn)
-#         turn = 'O'
-
-#     elif turn == "O":
-#         theBoard[move] = "O"
-#         checkwin(theBoard, turn)
-#         turn = 'X'
+    Args:
+        lx ([type]): [description]
+        lo ([type]): [description]1
+    >>> checwin([1,2,3], [2,4,5])
+    X win1
+    """
+    for i in check_list:
+        if board[f"{i[0]}"] == " " or board[f"{i[1]}"] == " " or board[f"{i[2]}"] == " ":
+            continue
+        elif board[f"{i[0]}"] == board[f"{i[1]}"] == board[f"{i[2]}"]:
+            printBoard(theBoard)
+            print(f"{turn} WIN!!!")
+            sys.exit()
 
 
-# printBoard(theBoard)
+turn = 'X'
+indexlist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# loop 9 time
+for i in range(9):
+    printBoard(the_num_Board)
+    print()
+    printBoard(theBoard)
+    print('Turn for ' + turn + '. Move on which space?')
+    # check move that is not letter or more than 9
+    while True:
+        move = input()
+        try:
+            o = int(move)
 
-a = [1, 2, 3, 4]
-b = a
-c = b + [6]
-c.sort(reverse=True)
-print(c)
+        except ValueError:
+            print("Incrorect type")
+            continue
+        try:
+            c = indexlist[o-1]
+            break
+        except IndexError:
+            print("Type number 1-9 ")
+            continue
+        theBoard[move] = turn
+        checkwin(theBoard, turn)
+    if turn == 'X':
+        turn = 'O'
+    elif turn == "O":
+        turn = 'X'
+
+
+printBoard(theBoard)
