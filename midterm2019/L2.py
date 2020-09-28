ac60d2d3000d53244773798e21353190588cb621
vitvara <vitvara1@gmail.com>
2020-09-28 22:54:18 +0700

    L2.2 Done!

--- midterm2019/L2.py
+++ midterm2019/L2.py
@@ -14,3 +14,20 @@ def num_even_digits(n):
         if int(i) % 2 == 0:
             count += 1
     print(count)
+
+
+def possible_rectangle(size1, size2, size3, size4):
+    """
+    >>> possible_rectangle(10, 20, 20, 10)
+    True
+    >>> possible_rectangle(10, 30, 20, 10)
+    False
+    >>> possible_rectangle(10, 10, 10, 10)
+    True
+    >>> possible_rectangle(10, 10, 20, 10)
+    False
+    """
+    if size1 == size4 and size2 == size3:
+        print(True)
+        return None
+    print(False)
