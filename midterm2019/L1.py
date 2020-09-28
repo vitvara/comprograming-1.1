32258c2feae4d23804d17f649734c17a585aa23f
vitvara <vitvara1@gmail.com>
2020-09-28 22:43:28 +0700

    L1 done!

--- midterm2019/L1.py
+++ midterm2019/L1.py
@@ -16,3 +16,24 @@ def all_equal_int(x1, x2, x3, x4):
                 print(False)
                 return None
     print(True)
+
+
+def circles_overlapping(x1, y1, x2, y2, r):
+    """
+    >>> circles_overlapping(0, 0, 2, 0, 2)
+    True
+    >>> circles_overlapping(2, 2, 4, 2, 0.7)
+    False
+    >>> circles_overlapping(1, 1, 2, 2, 0.6)
+    False
+    >>> circles_overlapping(1, 1, 4, 5, 3)
+    True
+    """
+    distance = ((x1 - x2)**2 + (y1 - y2)**2)**(1/2)
+    if (distance/2) <= r:
+        print(True)
+        return None
+    print(False)
+
+
+circles_overlapping(1, 1, 4, 5, 3)
