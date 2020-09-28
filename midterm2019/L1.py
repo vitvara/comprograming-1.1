977a1098992626718b720fbefe4f6c8340c5a0fd
vitvara <vitvara1@gmail.com>
2020-09-28 21:39:30 +0700

    L4.2 done!

--- midterm2019/L1.py
+++ midterm2019/L1.py
@@ -21,7 +21,27 @@ def group_elements(l):
             local_list = []
             used_num.append(i)
 
+    # print(return_list)
+    return return_list
+
+
+def dup_elements(l):
+    """
+    >>> dup_elements([1, 3, 3, 4, 2, 5, 8, 5, 6, 7])
+    [3, 5]
+    >>> dup_elements([3, 3, 3, 3])
+    [3]
+    >>> dup_elements([3, 3, 1, 3, 3, 1])
+    [3, 1]
+    >>> dup_elements([5, 3, 1, 2, 4, 10])
+    """
+    return_list = []
+    for i in group_elements(l):
+        if len(i) > 1:
+            return_list.append(i[0])
+    if return_list == []:
+        return
     print(return_list)
 
 
-group_elements([5, 3, 1, 2, 4, 10])
+dup_elements([1, 3, 3, 4, 2, 5, 8, 5, 6, 7])
